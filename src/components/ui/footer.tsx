a4250a4cd826715228ead36b51e29244cc182bb0
mtmty <mtmtyusei@hotmail.com>
2024-09-29 00:38:07 +0900

    フッターを編集

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -2,14 +2,17 @@ export default function SiteFooter() {
   return (
     <footer className="flex justify-center items-center text-gray-500 dark:text-gray-400 py-14">
       <p>
-        Made with{" "}
-        <span role="img" aria-label="love">
-          ☕
-        </span>{" "}
-        by{" "}
         <a href="https://github.com/PiratePeep" className="font-medium text-purple-400 dark:text-violet-500">
           PiratePeep
         </a>
+        によって作成されました。{" "}
+        <span role="img" aria-label="love">
+          ☕
+        </span>{" "}
+        <a href="https://github.com/mtmtyu" className="font-medium text-purple-400 dark:text-violet-500">
+          mtmtyu
+        </a>
+        によって編集・翻訳されました。
       </p>
     </footer>
   );
